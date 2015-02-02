3885704ab4cce0563b96bdf1107c637afdf3e9db
karelVanGeerdeghom <karel.van.geerdeghom@gmail.com>
2015-02-02 22:53:38 +0100

    work

--- OOP.mySvg.js
+++ OOP.mySvg.js
@@ -2,58 +2,63 @@
 
 $(function() {
     var eCanvas = document.getElementById('canvas');
+    var ePlayer = $('#playerStats');
+    
     var eSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     eSvg.setAttributeNS('http://www.w3.org/2000/svg','xlink','http://www.w3.org/1999/xlink');
-    eSvg.setAttributeNS(null, "width", 600);
-    eSvg.setAttributeNS(null, "height", 600);
+    eSvg.setAttributeNS(null, "width", 800);
+    eSvg.setAttributeNS(null, "height", 800);
     eCanvas.appendChild(eSvg);
     
-    var center = new Punt(300, 300);
-    var board = new Board(eSvg, center, 2, 32);
+    var center = new Punt(400, 400);
+    var board = new Board(eSvg, center, 3, 32);
     board.build();
     board.defs();
     board.draw();
-    var heart = board.find("0, 0");
-    board.rune(heart, "heart");
+    board.rune();
     var player = new Player();
-//    var circle1 = new Circle(eSvg, center, 1, 40);
-//    circle1.element();
-//    var circle2 = new Circle(eSvg, center, 1, 96);
-//    circle2.element();
-//    var circle3 = new Circle(eSvg, center, 1, 162);
-//    circle3.element();
-//    var heartcircle = new Circle(eSvg, center, 2, 26);
-//    heartcircle.element();
 
     $("polygon").on("click", function() {
-        var id = $(this).attr("id");
+        var id = parseInt($(this).attr("id"));
         var runes = ["dam", "as", "cc", "cd", "range", "gold"];
-        var hex = board.find(id);
-        hex.modifier += 1;
-        board.unrune(hex);
-        if (hex.modifier === 7) {
-            this.setAttribute("fill", "transparent");
-            hex.modifier = 0;
+        var hex = board.hexes[id];
+        if (id !== board.heart) {
+            var random = Math.floor(Math.random() * 4);
+            hex.rune = runes[random];
+            hex.runeSet = random;
+            board.unrune();
+            board.connect();
+            board.rune();
         }
-        else {
-            board.rune(hex, runes[hex.modifier - 1]);
-            this.setAttribute("fill", "url(#" + id.replace(", ", "") + ")"); 
-        }
-        var eStats = $("#playerStats");
-        eStats.empty();
-        var sDps = player.dps();
-        var eDps = ("<p>Player dps: " + sDps + "</p>");
-        eStats.append(eDps);
+        if (id === board.heart) {
+            ePlayer.empty();
+            var eHealth = $('<p>Player Health: ' + player.health + '</p>');
+            var eRadius = $('<p>Player Range: ' + player.radius + '</p>');
+            var eDamage = $('<p>Player Damage: ' + player.dam + '</p>');
+            var eAs = $('<p>Player Attack Speed: ' + player.as + ' / second</p>');
+            var eCc = $('<p>Player Critical Chance: ' + player.cc + '</p>');
+            var eCd = $('<p>Player Critical Damage: ' + player.cd + '</p>');
+            var eDps = $('<p>Total DPS: ' + board.dps(player) + '</p>');
+            var eRunes = $('<p>Runes: ' + board.runes + '</p>')
+            var eBoardDam = $('<p>Board Dam: ' + board.dam + '</p>');
+            var eBoardAs = $('<p>Board As: ' + board.as + '</p>');
+            var eBoardCc = $('<p>Board CC: ' + board.cc + '</p>');
+            var Cd = $('<p>Board CD: ' + board.cd + '</p>');
+            
+            ePlayer.append(eHealth)
+                .append(eRadius)
+                .append(eDamage)
+                .append(eAs)
+                .append(eCc)
+                .append(eCd)
+                .append(eDps)
+                .append(eRunes)
+                .append(eBoardDam)
+                .append(eBoardAs)
+                .append(eBoardCc)
+                .append(Cd)
 
-    });
-    $("polygon").on("mouseover", function() {
-        var id = $(this).attr("id");
-        var hex = board.find(id);
-        var eStats = $("#hexStats");
-        eStats.empty();
-        var sModifier = hex.modifier;
-        var eModifier = ("<p>Modifier: " + sModifier + "</p>");
-        eStats.append(eModifier);
+        }
     });
 });
 
@@ -62,31 +67,23 @@ function Punt(x, y) {
     this.y = y;
 }
 
-function Circle(canvas, center, width, radius) {
-    this.canvas = canvas;
-    this.center = center;
-    this.width = width;
-    this.radius = radius;
-}
-Circle.prototype.element = function() {
-//    <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
-    this.element = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-    this.element.setAttribute('cx', this.center.x);
-    this.element.setAttribute('cy', this.center.y);
-    this.element.setAttribute('r', this.radius);
-    this.element.setAttribute('stroke', 'red');
-    this.element.setAttribute('stroke-width', this.width);
-    this.element.setAttribute("fill", "none");
-//    return this.element;
-    this.canvas.appendChild(this.element);
-}
-
-function Hex(id, center, side){
+function Hex(id, xid, yid, center, side){
     this.modifier = 0;
     this.center = center;
     this.side = side;
     this.height = Math.sqrt(3 * this.side * this.side);
     this.id = id;
+    this.xid = xid;
+    this.yid = yid;
+    this.rune = "";
+    this.runeSet = null;
+    this.rarity = 0;
+    this.connections = [];
+    this.directions = [];
+    this.dam = 0;
+    this.as = 0;
+    this.cc = 0;
+    this.cd = 0;
 }
 Hex.prototype.element = function() {
     this.points = "";
@@ -102,15 +99,14 @@ Hex.prototype.element = function() {
     this.element.setAttribute('stroke', '#444');
     this.element.setAttribute('stroke-width', '1px');
     this.element.setAttribute('fill', 'transparent');
-    if (this.id === "0, 0") { this.element.setAttribute('fill', 'url(#00)'); }
     this.element.setAttribute('id', this.id);
     this.element.setAttribute('class', 'hex');
     
     return this.element;
 };
-Hex.prototype.pattern = function(rune) {
+Hex.prototype.pattern = function() {
     this.pattern = document.createElementNS('http://www.w3.org/2000/svg', 'pattern');
-    this.pattern.setAttribute("id", this.id.replace(", ", ""));
+    this.pattern.setAttribute("id", "pattern" + this.id);
     this.pattern.setAttribute("patternUnits", "userSpaceOnUse");
     this.pattern.setAttribute("x", this.center.x - 1.8 * this.side);
     this.pattern.setAttribute("y", this.center.y - 1.8 * this.side);
@@ -120,20 +116,26 @@ Hex.prototype.pattern = function(rune) {
     var iRune = document.createElementNS('http://www.w3.org/2000/svg', 'image');
     iRune.setAttribute("width", this.side * 3.6);
     iRune.setAttribute("height", this.side * 3.6);
-    iRune.setAttributeNS('http://www.w3.org/1999/xlink', 'href', 'images/' + rune + '.png');
-    var iCircle = document.createElementNS('http://www.w3.org/2000/svg', 'image');
-    iCircle.setAttribute("width", this.side * 3.6);
-    iCircle.setAttribute("height", this.side * 3.6);
-    iCircle.setAttributeNS('http://www.w3.org/1999/xlink', 'href', 'images/circlefull.png');
-
+    iRune.setAttributeNS('http://www.w3.org/1999/xlink', 'href', 'images/rune-' + this.rune + + this.rarity + '.png');
     this.pattern.appendChild(iRune);
-    this.pattern.appendChild(iCircle);
-
-//    var image3 = document.createElementNS('http://www.w3.org/2000/svg', 'image');
-//    image3.setAttribute("width", this.side * 3.6);
-//    image3.setAttribute("height", this.side * 3.6);
-//    image3.setAttributeNS('http://www.w3.org/1999/xlink', 'href', 'images/right.png');
-//    this.pattern.appendChild(image3);
+    
+    if (this.connections.length > 0 || this.rune === "heart") {
+        var iCircle = document.createElementNS('http://www.w3.org/2000/svg', 'image');
+        iCircle.setAttribute("width", this.side * 3.6);
+        iCircle.setAttribute("height", this.side * 3.6);
+        iCircle.setAttributeNS('http://www.w3.org/1999/xlink', 'href', 'images/conncircle.png');
+        this.pattern.appendChild(iCircle);
+    }
+    
+    for (var i = 0; i < this.connections.length; i++) {
+        var iConnect = document.createElementNS('http://www.w3.org/2000/svg', 'image');
+        iConnect.setAttribute("width", this.side * 3.6);
+        iConnect.setAttribute("height", this.side * 3.6);
+        iConnect.setAttributeNS('http://www.w3.org/1999/xlink', 'href', 'images/conn' + this.directions[i] + '.png');
+        this.pattern.appendChild(iConnect);
+    }
+    
+    this.element.setAttribute("fill", "url(#pattern" + this.id + ")");
     
     return this.pattern;
 };
@@ -145,24 +147,29 @@ function Board(canvas, center, radius, side) {
     this.side = side;
     this.height = Math.sqrt(3 * this.side * this.side);
     this.hexes = [];
+    this.runes = [];
+    this.runeStats = [1, 2, 1, 10];
 };
 Board.prototype.defs = function() {
     this.defs = document.createElementNS('http://www.w3.org/2000/svg', 'defs');
     this.canvas.appendChild(this.defs);
 };
 Board.prototype.build = function() {
-    for (var j = -this.radius; j <= this.radius; j++) {
-        var nI = parseInt(this.radius - Math.floor(Math.abs(j) / 2));
-        var nJ = -Math.abs(j % 2);
-        for (var i = -nI; i <= nI; i++) {
-            if (!(i === 0 && nJ !== 0)) {
-                var aId = i + ", " + -j;
-                var offSet = 0;
-                if (nJ !== 0) { var offSet = nJ * i / Math.abs(i); }
-                var punt = new Punt(this.center.x + this.height * (2 * i + offSet), this.center.y + 3 * j * this.side);
-                var hex = new Hex(aId, punt, this.side);
-                this.hexes.push(hex);
+    var x = this.center.x - this.height * (this.radius + this.radius);
+    var y = this.center.y - this.side * this.radius * 3; 
+    for (var j = 0; j <= this.radius * 2; j++) {
+        var offSet = Math.abs(j - this.radius);
+        var xId = 0;
+        if(j - this.radius > 0) {
+            xId = j - this.radius;
+        }
+        for (var i = 0; i <= this.radius * 2 - offSet; i++) {
+            var punt = new Punt(x + this.height * (2 * i + offSet), y + 3 * j * this.side);
+            var hex = new Hex(this.hexes.length, i + xId, j, punt, this.side);
+            if (i === this.radius && j === this.radius) {
+                this.heart = this.hexes.length;
             }
+            this.hexes.push(hex);
         }
     }
 };
@@ -171,28 +178,117 @@ Board.prototype.draw = function() {
         this.canvas.appendChild(this.hexes[i].element());
     }
 };
-Board.prototype.unrune = function(hex) {
-    delete hex["image"];
-    delete hex["pattern"];
-    $('pattern#' + hex.id.replace(", ", "")).remove();
-};
-Board.prototype.rune = function(hex, rune) {
-    this.defs.appendChild(hex.pattern(rune));
-};
-Board.prototype.find = function(id) {
+Board.prototype.connect = function() {
     for (var i = 0; i < this.hexes.length; i++ ) {
-        if (this.hexes[i].id === id) { return this.hexes[i]; }
+        for (var j = 0; j < this.hexes.length; j++ ) {
+            if (this.hexes[i].rune !== "" && this.hexes[j].rune !== "") {
+                if(this.hexes[i].xid == this.hexes[j].xid - 1 && this.hexes[i].yid == this.hexes[j].yid) {
+                    if (this.hexes[i].connections.indexOf(j) === -1) { this.hexes[i].connections.push(j); }
+                    if (this.hexes[i].directions.indexOf(1) === -1) { this.hexes[i].directions.push(1); }
+                }
+                if(this.hexes[i].xid == this.hexes[j].xid + 1 && this.hexes[i].yid == this.hexes[j].yid) {
+                    if (this.hexes[i].connections.indexOf(j) === -1) { this.hexes[i].connections.push(j); }
+                    if (this.hexes[i].directions.indexOf(4) === -1) { this.hexes[i].directions.push(4); }
+                }
+                if(this.hexes[i].xid == this.hexes[j].xid + 1 && this.hexes[i].yid == this.hexes[j].yid + 1) {
+                    if (this.hexes[i].connections.indexOf(j) === -1) { this.hexes[i].connections.push(j); }
+                    if (this.hexes[i].directions.indexOf(5) === -1) { this.hexes[i].directions.push(5); }
+                }
+                if(this.hexes[i].xid == this.hexes[j].xid - 1 && this.hexes[i].yid == this.hexes[j].yid - 1) {
+                    if (this.hexes[i].connections.indexOf(j) === -1) { this.hexes[i].connections.push(j); }
+                    if (this.hexes[i].directions.indexOf(2) === -1) { this.hexes[i].directions.push(2); }
+                }
+                if(this.hexes[i].xid == this.hexes[j].xid && this.hexes[i].yid == this.hexes[j].yid + 1) {
+                    if (this.hexes[i].connections.indexOf(j) === -1) { this.hexes[i].connections.push(j); }
+                    if (this.hexes[i].directions.indexOf(6) === -1) { this.hexes[i].directions.push(6); }
+                }
+                if(this.hexes[i].xid == this.hexes[j].xid && this.hexes[i].yid == this.hexes[j].yid - 1) {
+                    if (this.hexes[i].connections.indexOf(j) === -1) { this.hexes[i].connections.push(j); }
+                    if (this.hexes[i].directions.indexOf(3) === -1) { this.hexes[i].directions.push(3); }
+                }
+            }
+        }
     }
+};
+Board.prototype.unrune = function() {
+    for (var i = 0; i < this.hexes.length; i++ ) {
+        delete this.hexes[i]["pattern"];
+        $('#pattern' + this.hexes[i].id).remove();
+    }
+};
+Board.prototype.rune = function() {
+    this.runes = [];
+    for (var i = 0; i < this.hexes.length; i++ ) {
+        this.hexes[i].dam = 0;
+        this.hexes[i].as = 0;
+        this.hexes[i].cc = 0;
+        this.hexes[i].cd = 0;
+        if (this.hexes[i].xid === this.radius && this.hexes[i].yid === this.radius) {
+            this.hexes[i].rune = "heart";
+        }
+        if (this.hexes[i].rune !== "") {
+            this.defs.appendChild(this.hexes[i].pattern());
+            if (this.hexes[i].rune !== "heart") {
+                this.runes.push(i);
+            }
+            switch (this.hexes[i].runeSet) {
+                case 0:
+                    this.hexes[i].dam = this.runeStats[0];
+                    break;
+                case 1:
+                    this.hexes[i].as = this.runeStats[1];
+                    break;
+                case 2:
+                    this.hexes[i].cc = this.runeStats[2];
+                    break;
+                case 3:
+                    this.hexes[i].cd = this.runeStats[3];
+                    break;
+            }
+        }
+    }
+};
+Board.prototype.dps = function(player) {
+    this.dam = 0;
+    this.as = 0;
+    this.cc = 0;
+    this.cd = 0;
+    for (var i = 0; i < this.runes.length; i++) {
+        var hex = this.hexes[this.runes[i]];
+        var asc;
+        var desc;
+        switch (hex.connections.length) {
+            case 0: asc = 0; break;
+            case 1: asc = 0.2; desc = 1.2; break;
+            case 2: asc = 0.4; desc = 1; break;
+            case 3: asc = 0.6; desc = 0.8; break;
+            case 4: asc = 0.8; desc = 0.6; break;
+            case 5: asc = 1; desc = 0.4; break;
+            case 6: asc = 1.2; desc = 0.2; break;
+        }
+        this.dam += hex.dam * asc;
+        this.as += hex.as * asc;
+        this.cc += hex.cc * desc;
+        this.cd += hex.cd * desc;
+    }
+    var dam = player.dam + (this.dam / 100);
+    var as = player.as + (this.as / 100);
+    var cc = player.cc + this.cc;
+    var cd = player.cd + this.cd;
+    var playerDps = (player.dam * player.cc * (100 + player.cd) / 10000 + player.dam * (100 - player.cc) / 100) * player.as;
+    var totalDps = (dam * cc * (100 + cd) / 10000 + dam * (100 - cc) / 100) * as;
+    var diff = totalDps / playerDps;
+//    diff = diff * diff * diff * diff * diff * diff * diff * diff * diff * diff;
+//    diff = diff - 1;
+//    var total = diff * (this.runes.length + 1);
+    return "" + diff * diff + "";
 };
 
 function Player() {
     this.health = 100;
     this.radius = 100;
-    this.dam = 100;
+    this.dam = 1;
     this.cc = 5;
     this.cd = 50;
     this.as = 2;
-};
-Player.prototype.dps = function() {
-    return this.dam * this.cc * (100 + this.cd) / 10000 + this.dam * (100 - this.cc) / 100;
 };
